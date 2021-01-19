aed7c036e532ecd74400e6003a15e8b43bb7483a
Fabio Petrone <fa610movic90@gmail.com>
2021-01-19 17:32:10 +0100

    ..

--- es6/js/script.js
+++ es6/js/script.js
@@ -40,54 +40,6 @@ console.log(`La bici leggera è quella ${nome} che ha peso ${peso}`);
 // SNACK2
 
 
-// SNACK2
-//
-// Creare un array di oggetti di squadre di calcio.
-// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-//
-// const squadre = [
-//   {
-//     nome: 'Inter',
-//     puntifatti: 0,
-//     fallisubiti: 0
-//   },
-//   {
-//     nome: 'Milan',
-//     puntifatti: 0,
-//     fallisubiti: 0
-//   },
-//   {
-//     nome: 'Juve',
-//     puntifatti: 0,
-//     fallisubiti: 0
-//   },
-//   {
-//     nome: 'Napoli',
-//     puntifatti: 0,
-//     fallisubiti: 0
-//   }
-// ];
-// // le arrow functions le vedremo domani vabbè
-// // const RandomNumber = (base) => Math.floor(Math.random() * base) + 1;
-// for (let i = 0; i < squadre.length; i++) {
-//     squadre[i].puntifatti=Math.floor(Math.random()*100+1);
-//     squadre[i].fallisubiti=Math.floor(Math.random()*100+1);
-//   }
-//      //
-//      // squadre[i].puntifatti=RandomNumber(100);
-//      // squadre[i].fallisubiti=RandomNumber(100);
-//
-// for (let i = 0; i <squadre.length; i++) {
-// const{nome,fallisubiti}=squadre[i];
-// console.log(nome,fallisubiti);
-// }
-//
-// // reference:https://medium.com/@seanmcp/js-basics-random-number-680fa269c4d7
-
-
 const squadre = [
   {
     nome: 'Inter',
@@ -128,8 +80,6 @@ for (let i = 0; i < squadre.length; i++) {
    // squadreB.push({nome: nome,
    // falli_subiti:falli_subiti});
    squadreB.push({nome,falli_subiti});
-
-
 
 }
 console.log(squadreB);
