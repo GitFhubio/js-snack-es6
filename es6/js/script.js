2390ed36845e5eee5d81ea4ed7a5a2c8271e3bb1
Fabio Petrone <fa610movic90@gmail.com>
2021-01-19 14:52:45 +0100

    esercizio2 in es6

--- es6/js/script.js
+++ es6/js/script.js
@@ -32,19 +32,54 @@ for (let i = 1; i < bici.length; i++) {
    }
 }
 
-// oppure
-// var bicileggera;
-// for (var i = 0; i < bici.length-1; i++) {
-//    if (bici[i].peso>bici[i+1].peso){
-//     bicileggera=bici[i+1];
-//    }
-// }
 
 console.log(bicileggera);
 
 
-// correzione ottavio
-
+// SNACK2
 
 
 // SNACK2
+//
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+const squadre = [
+  {
+    'nome': 'Inter',
+    'punti fatti': 0,
+    'falli subiti': 0
+  },
+  {
+    'nome': 'Milan',
+    'punti fatti': 0,
+    'falli subiti': 0
+  },
+  {
+    'nome': 'Juve',
+    'punti fatti': 0,
+    'falli subiti': 0
+  },
+  {
+    'nome': 'Napoli',
+    'punti fatti': 0,
+    'falli subiti': 0
+  }
+];
+// le arrow functions le vedremo domani vabbè
+// const RandomNumber = (base) => Math.floor(Math.random() * base) + 1;
+for (let i = 0; i < squadre.length; i++) {
+    squadre[i]['punti fatti']=Math.floor(Math.random()*100+1);
+    squadre[i]['falli subiti']=Math.floor(Math.random()*100+1);
+     //
+     // squadre[i]['punti fatti']=RandomNumber(100);
+     // squadre[i]['falli subiti']=RandomNumber(100);
+
+
+}
+console.log(squadre);
+// oppure
