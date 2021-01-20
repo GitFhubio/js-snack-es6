23b4d52938c2d12f24faad209386c3359523077c
Fabio Petrone <fa610movic90@gmail.com>
2021-01-20 14:19:50 +0100

    .

--- es6/2/js/script.js
+++ es6/2/js/script.js
@@ -2,7 +2,7 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
-function TraIndici(min,max,array){
+function traIndici(min,max,array){
 
 const FilteredArray=array.filter((element,index,array)=>{
   return index<=max && index>=min;
@@ -11,5 +11,5 @@ const FilteredArray=array.filter((element,index,array)=>{
 return FilteredArray;
 }
 
-const arr=TraIndici(2,5,[1,2,4,88,67,3,7,5,4]);
+const arr=traIndici(2,5,[1,2,4,88,67,3,7,5,4]);
 console.log(arr);
