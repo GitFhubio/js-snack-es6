2f0b3a5d0d405a402fbd9c09021f2642ac18bf1d
Fabio Petrone <fa610movic90@gmail.com>
2021-01-20 15:57:18 +0100

    ..

--- es6/2/js/script.js
+++ es6/2/js/script.js
@@ -2,6 +2,24 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
+let minimo;
+let massimo;
+const arr1=['giacomo','peppe','filippo','antonio','giovanni'];
+
+
+ let input1=parseInt(prompt('Inserisci primo valore'));
+
+ let input2=parseInt(prompt('Inserisci secondo valore'));
+
+if(input2<input1){
+minimo=input2;
+massimo=input1;
+
+} else if(input1<input2){
+massimo=input2;
+minimo=input1;
+}
+
 function traIndici(min,max,array){
 
 const FilteredArray=array.filter((element,index,array)=>{
--- vanilla/2/js/script.js
+++ vanilla/2/js/script.js
@@ -2,16 +2,32 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
+var minimo;
+var massimo;
+var arr1=['giacomo','peppe','filippo','antonio','giovanni'];
+
+ var input1=parseInt(prompt('Inserisci primo valore'))}
+
+ var input2=parseInt(prompt('Inserisci secondo valore'));
+
+if(input2<input1){
+minimo=input2;
+massimo=input1;
+
+} else if(input1<input2){
+massimo=input2;
+minimo=input1;
+}
 function traIndici(min,max,arrayIniziale)
 {
-var arrayFinale=[];
+var arr2=[];
 while(min<=max)
 {
-   arrayFinale.push(arrayIniziale[min]);
+   arr2.push(arrayIniziale[min]);
    min++;
 }
-return arrayFinale;
+return arr2;
 }
 
-var arr=traIndici(2,5,[1,2,4,5,6,3,7,5,4]);
-console.log(arr);
+var arr2=traIndici(minimo,massimo,arr1);
+console.log(arr2);
