c626945323d3c36c0217017fe706b91e2d0e0a77
Fabio Petrone <fa610movic90@gmail.com>
2021-01-20 17:39:01 +0100

    con condizioni

--- es6/2/js/script.js
+++ es6/2/js/script.js
@@ -2,23 +2,24 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
-let minimo;
-let massimo;
+
 const arr1=['giacomo','peppe','filippo','antonio','giovanni'];
- let input1=parseInt(prompt('Inserisci primo valore'));
- let input2=parseInt(prompt('Inserisci secondo valore'));
-
-if(input2<input1){
-minimo=input2;
-massimo=input1;
-
-} else if(input1<input2){
-massimo=input2;
-minimo=input1;
+let input1=-1;
+let input2=-1;
+while(input1<0 || input1>arr1.length){
+input1=parseInt(prompt('Inserisci minimo'));
+if (input1<0 || input1>=arr1.length){
+  alert('Devi inserire un valore tra 0 e'+(arr1.length-1))
+}
+}
+while(input2<=input1 || input2>=arr1.length){
+input2=parseInt(prompt('Inserisci massimo'));
+if (input2<0 || input2>=arr1.length){
+  alert('Devi inserire un valore tra '+input1+' e'+(arr1.length-1))
 }
 
+}
 function traIndici(min,max,array){
-
 const FilteredArray=array.filter((element,index,array)=>{
   return index<=max && index>=min;
 });
@@ -26,4 +27,118 @@ const FilteredArray=array.filter((element,index,array)=>{
 return FilteredArray;
 }
 
-console.log(traIndici(minimo,massimo,['giacomo','peppe','filippo','antonio','giovanni']));
+console.log(traIndici(input1,input2,arr1));
+
+
+//
+// const arr1=['giacomo','peppe','filippo','antonio','giovanni','alfredo','archimede'];
+//  const minimo=parseInt(prompt('Inserisci minimo'))}
+//  const massimo=parseInt(prompt('Inserisci massimo'));
+//
+// function traIndici(min,max,arrayIniziale)
+// {
+// const arr2=[];
+//
+// // for(let i=0;i>array.length,i++){
+// //  if(i>=min && i<=max)
+// // {
+// //   arr2.push(arrayIniziale[i]);
+// // }
+// // }
+//
+// arrayIniziale.forEach((elem,index) =>{
+//   if(index>=min && i<=max)
+//   {
+//     arr2.push(elem;);
+//   }
+// }
+//
+// return arr2;
+// }
+//
+// console.log(traIndici(minimo,massimo,arr1));
+
+// note
+// il filter a differenza del foreach returna un array
+// map va bene quando voglio modificare valori elementi array
+
+
+
+// ulitità map non è per filtrare;
+
+// function arrayToUpperCase(array){
+//
+// const newArray=array.map((elem)=>{
+// return elem.toUpperCase();
+// });
+//
+// return newArray;
+// console.log()
+
+// es5
+// function somma(a,b){
+//   return a+b;
+// }
+// somma(1,2);
+// // es6
+// const somma=(a,b) =>{return a+b}
+// somma(1,2);
+
+
+//operator spread per array
+//
+// const arrayInvitati=["Mario","Michela","Fabio"];
+// const arraySpecialGuest=["Giovanna","Emanuela","Carlo"];
+//
+// arraySpecialGuest.forEach((element,index)=>
+// {
+//   arrayInvitati.push(elem);
+// }
+//
+//
+// arrayInvitati=[...arrayInvitati,...arraySpecialGuest];
+
+// const arrayPartecipanti=["Maria","Fabio","Carlo","Vito"];
+// const arrayNonPartecipanti=["Marco","Mirco","Giuseppe"];
+// const invitati=[...arrayPartecipanti,...arrayNonPartecipanti];
+// const arrayIndecisi=["Adele","Teresa","Gianpaolo"];
+// l\'alternativa sarebbe stata
+//
+// const invitati=[];
+//
+// arrayPartecipanti.forEach((element) =>{
+//   invitati.push(elem);
+// }
+//
+// )
+// arrayNonPartecipanti.forEach((element) =>{
+//   invitati.push(elem);
+// }
+//
+// )
+
+// spread operator per oggetti
+
+// let persona={
+//   nome:'Marco',
+//   cognome:'Rossi',
+//   eta:25
+// }
+// deve avere stesse proprietà piu altre due
+
+// let personaInvitata {
+//
+// nome:persona.nome,
+// cognome:persona.cognome,
+// eta:persona.eta,
+// invitato:true,
+// partecipera:false
+// }
+//
+// let personaInvitata {
+// ...persona, //ho creato copia  di fatto cui ho aggiunto altre proprietà
+// invitato:true,
+// partecipera:false
+// }
+//
+// prende tutte proprietà di persona e poi ne aggiunge due
--- vanilla/2/js/script.js
+++ vanilla/2/js/script.js
@@ -2,20 +2,22 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
-var minimo;
-var massimo;
-var arr1=['giacomo','peppe','filippo','antonio','giovanni'];
- var input1=parseInt(prompt('Inserisci primo valore'))}
- var input2=parseInt(prompt('Inserisci secondo valore'));
-
-if(input2<input1){
-minimo=input2;
-massimo=input1;
-
-} else if(input1<input2){
-massimo=input2;
-minimo=input1;
+var arr1=['giacomo','peppe','filippo','antonio','giovanni','alfredo','archimede'];
+var minimo=-1;
+var massimo=-1;
+while(minimo<0 || minimo>=arr1.length){
+minimo=parseInt(prompt('Inserisci minimo'));
+if (minimo<0 || minimo>=arr1.length){
+  alert('Devi inserire un valore tra 0 e'+(arr1.length-1))
 }
+}
+while(massimo<=minimo || massimo>=arr1.length){
+ var massimo=parseInt(prompt('Inserisci massimo'));
+ if (massimo<0 || massimo>=arr1.length){
+   alert('Devi inserire un valore tra '+minimo+' e'+(arr1.length-1));
+ }
+}
+
 function traIndici(min,max,arrayIniziale)
 {
 var arr2=[];
@@ -24,6 +26,14 @@ while(min<=max)
    arr2.push(arrayIniziale[min]);
    min++;
 }
+// alternativa in cui ciclo tutti gli elementi
+// for(var i=0;i>array.length,i++){
+//  if(i>=min && i<=max)
+// {
+//   arr2.push(arrayIniziale[i]);
+// }
+// }
+
 return arr2;
 }
 
